83d9e15b7b75949b41c979aeae2dad7ea03c50f5
Becca Aaronson <becca.aaronson@gmail.com>
2015-05-08 11:56:23 -0500

    testing things

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,34 @@
 'use strict';
 
+var menuPosition = function() {
+  var topPos = $('#side-nav').offset().top;
+  var headerHeight = $('#styleguide-header').height();
+
+  // if ($(window).scrollTop() > topPos) {
+  //   $('#side-nav').css({position: 'fixed', top: '0'});
+  // } else {
+
+  // }
+  $.when($(window).scrollTop() > topPos).then(function(){
+    if ($(window).width() > 600) {
+      $('#side-nav').css({position: 'fixed', top: '0'});
+    }
+  });
+
+
+};
+
 $(document).ready(function(){
   // Hide-Show code blocks
   $('.code-block > .button').click(function(){
       $(this).next('pre').toggle();
   });
+
+  menuPosition();
+});
+
+
+
+$(window).scroll(function(){
+  menuPosition();
 });
